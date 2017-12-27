f56434ad19eac1ea57a63c3f4b730a316bd5c42f
AJRaffles <pietomb00@gmail.com>
2017-12-27 16:26:58 +0000

    Initial commit of template plugin

---
+++ src/main/java/co/uk/ctdev/tidyelastic/TidyPlugin.java
@@ -0,0 +1,9 @@
+package co.uk.ctdev.tidyelastic;
+
+import org.elasticsearch.plugins.Plugin;
+
+/**
+ * TidyPlugin.
+ */
+public class TidyPlugin extends Plugin {
+}
---
+++ src/test/java/co/uk/ctdev/tidyelastic/TidyPluginTest.java
@@ -0,0 +1,37 @@
+package co.uk.ctdev.tidyelastic;
+
+import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
+import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.PluginInfo;
+import org.elasticsearch.test.ESIntegTestCase;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.is;
+
+/**
+ * TidyPluginTest.
+ */
+public class TidyPluginTest extends ESIntegTestCase {
+
+    @Override
+    protected Collection<Class<? extends Plugin>> nodePlugins() {
+        return Collections.singleton(TidyPlugin.class);
+    }
+
+    public void testPluginIsLoaded() throws Exception {
+        NodesInfoResponse response = client().admin().cluster().prepareNodesInfo().setPlugins(true).get();
+        for (NodeInfo nodeInfo : response.getNodes()) {
+            boolean pluginFound = false;
+            for (PluginInfo pluginInfo : nodeInfo.getPlugins().getPluginInfos()) {
+                if (pluginInfo.getName().equals(TidyPlugin.class.getName())) {
+                    pluginFound = true;
+                    break;
+                }
+            }
+            assertThat(pluginFound, is(true));
+        }
+    }
+}
